4f2f68afc30b2ade150eda9240369f138efe4d8b
SoCMo <247805431@qq.com>
2020-03-30 16:44:39 +0800

    add: BaiduTool
    Signed-off-by: SoCMo <247805431@qq.com>

--- src/test/java/com/nCov/DataView/NCovApplicationTests.java
+++ src/test/java/com/nCov/DataView/NCovApplicationTests.java
@@ -1,20 +1,24 @@
 package com.nCov.DataView;
 
-import com.nCov.DataView.tools.FixTool;
+import com.nCov.DataView.exception.AllException;
+import com.nCov.DataView.tools.BaiduTool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.text.ParseException;
+import java.io.IOException;
 
 @SpringBootTest
 class NCovApplicationTests {
     @Resource
-    private FixTool fixTool;
+    private BaiduTool baiduTool;
 
     @Test
-    void contextLoads() throws ParseException {
-        fixTool.dataFix();
+    void contextLoads() throws IOException, AllException {
+//        baiduTool.geoCoding("天台县汇泉东路5-7")
+//                .values()
+//                .forEach(System.out::println);
+
     }
 
 }
